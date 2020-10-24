00d93e55c943515ea289918c85c2d11a1ddcb63a
Viktor Horvath <viktor.horvath@wyss.harvard.edu>
2020-10-24 11:54:37 -0400

    Making offset available for air_get()

--- R/airtabler.R
+++ R/airtabler.R
@@ -89,6 +89,7 @@ air_get <- function(base, table_name, record_id = NULL,
   )
   air_validate(res)      # throws exception (stop) if error
   ret <- air_parse(res)  # returns R object
+  offset <- attr(ret, "offset")
   if(combined_result && is.null(record_id)) {
     # combine ID, Fields and CreatedTime in the same data frame:
     ret <-
@@ -97,6 +98,7 @@ air_get <- function(base, table_name, record_id = NULL,
         stringsAsFactors =FALSE
       )
   }
+  attr(ret, "offset") <- offset  
   ret
 }
 
