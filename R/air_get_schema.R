05cfc6ad3743cf0817976722f4163c5fc9f04da2
Collin Schwantes <schwantes@ecohealthalliance.org>
2021-07-06 13:13:07 -0500

    added a provisional function for querying the meta data api

--- R/air_get_schema.R
+++ R/air_get_schema.R
@@ -1,13 +1,27 @@
+#' Get base schema
+#'
+#' Get the schema for the tables in a base.
+#'
+#' @section Using Metadata API:
+#' The meta data api is not accepting new requests for client secrets as of
+#' 06 July 2021. Will update when metadata api becomes available.
+#'
+#' @param base Airtable base ID
+#' @param ... additional paramters
+#'
+#' @return list of schema
+#' @export air_get_schema
+
 air_get_schema <-  function(base,...){
   request_url <- sprintf("%s/%s/tables", air_meta_url, base)
-  request_url <- URLencode(request_url)
+  request_url <- utils::URLencode(request_url)
 
   # call service:
   res <- httr::GET(
     request_url,
     httr::add_headers(
       Authorization = paste("Bearer", air_api_key()),
-      X-Airtable-Client-Secret = air_secret_key()
+      "X-Airtable-Client-Secret" = air_secret_key()
     )
   )
 
